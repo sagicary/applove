29a94f0c43b61b0dbc715a884dd8b3772988f9a2
Sagicary <sagicary@gmail.com>
2017-05-31 23:21:35 -0400

    Listo html y css falta modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+ var imgclick=document.getElementsByClassName('imagen')[0];
+ console.log (imgclick);
+ imgclick.addEventListener("click", function(){
+ 	alert("Hizo click");
+ 	console.log("Paso por aqui");
+ });
